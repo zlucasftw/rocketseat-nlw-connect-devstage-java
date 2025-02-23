a0eb19bacfdfb44eaba44c5aca8e20ea75d33abb
zlucasftw <ilucasmcpvp@gmail.com>
2025-02-23 02:27:44 -0300

    first commit

---
+++ events/src/test/java/br/com/nlw/events/EventsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nlw.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
